ab2b0f02e141e801187eb77824db6da5f85fa7d1
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-04-16 21:18:57 -0700

    initial sketch

--- sklearn/impute/_iterative.py
+++ sklearn/impute/_iterative.py
@@ -6,7 +6,7 @@ import warnings
 from scipy import stats
 import numpy as np
 
-from ..base import clone
+from ..base import clone, is_classifier, BaseEstimator
 from ..exceptions import ConvergenceWarning
 from ..preprocessing import normalize
 from ..utils import (check_array, check_random_state, _safe_indexing,
@@ -290,9 +290,6 @@ class IterativeImputer(_BaseImputer):
             raise ValueError("If fit_mode is False, then an already-fitted "
                              "estimator should be passed in.")
 
-        if estimator is None:
-            estimator = clone(self._estimator)
-
         missing_row_mask = mask_missing_values[:, feat_idx]
         if fit_mode:
             X_train = _safe_indexing(X_filled[:, neighbor_feat_idx],
@@ -340,6 +337,7 @@ class IterativeImputer(_BaseImputer):
         # update the feature
         X_filled[missing_row_mask, feat_idx] = imputed_values
         return X_filled, estimator
+        
 
     def _get_neighbor_feat_idx(self,
                                n_features,
@@ -547,6 +545,107 @@ class IterativeImputer(_BaseImputer):
             )
         return limit
 
+    def initialize_iteration(self, X):
+        self.random_state_ = getattr(self, "random_state_",
+                                     check_random_state(self.random_state))
+
+        if self.max_iter < 0:
+            raise ValueError(
+                "'max_iter' should be a positive integer. Got {} instead."
+                .format(self.max_iter))
+
+        self.imputation_sequence_ = []
+
+        self.initial_imputer_ = None
+        super()._fit_indicator(X)
+        X_indicator = super()._transform_indicator(X)
+        X, Xt, self.mask_missing_values_ = self._initial_imputation(X)
+
+        if self.estimator is None:
+            # default estimator
+            from ..linear_model import BayesianRidge
+            self._estimators = [BayesianRidge() for _ in range(X.shape[1])]
+        elif isinstance(self.estimator, BaseEstimator):
+            # single estimator
+            self._estimators = [clone(self.estimator) for _ in range(X.shape[1])]
+        else:
+            # iterator of estimators
+            self._estimators = [clone(estimator) for estimator in self.estimator]
+        
+        has_classification_problem = False
+        for estimator in self._estimators:
+            if hasattr(estimator, 'random_state'):
+                estimator.random_state = self.random_state_
+            if is_classifier(estimator):
+                has_classification_problem = True
+    
+        if self.tol is not None and self.tol < 0:
+            raise ValueError(
+                "'tol' should be a non-negative float or None. Got {} instead."
+                .format(self.tol)
+            )
+        elif has_classification_problem:
+            raise ValueError(
+                    "`tol` paremeter is only valid for pure regression"
+                )
+
+        if self.sample_posterior and has_classification_problem:
+            raise ValueError(
+                "`sample_posterior` paremeter is only valid for pure regression"
+            )
+
+        # check min and max
+        self._min_value = IterativeImputer._validate_limit(
+            self.min_value, "min", X.shape[1])
+        self._max_value = IterativeImputer._validate_limit(
+            self.max_value, "max", X.shape[1])
+
+        if not np.all(np.greater(self._max_value, self._min_value)):
+            raise ValueError(
+                "One (or more) features have min_value >= max_value.")
+
+        # set iteration
+        self.n_iter_ = 0
+
+        # order in which to impute
+        # note this is probably too slow for large feature data (d > 100000)
+        # and a better way would be good.
+        # see: https://goo.gl/KyCNwj and subsequent comments
+        self.ordered_idx_ = self._get_ordered_idx(self.mask_missing_values_)
+        self.n_features_with_missing_ = len(self.ordered_idx_)
+
+        self.abs_corr_mat_ = self._get_abs_corr_mat(Xt)
+
+        # return results of initial imputation
+        return X, Xt, X_indicator
+
+    def next_iteration(self, X):
+        # X = Xt = X transformed
+        n_samples, n_features = X.shape
+
+        if self.imputation_order == 'random':
+            ordered_idx = self._get_ordered_idx(self.mask_missing_values_)
+        else:
+            ordered_idx = self.ordered_idx_
+        
+        for feat_idx in ordered_idx:
+
+            estimator = self._estimators[feat_idx]
+
+            neighbor_feat_idx = self._get_neighbor_feat_idx(n_features,
+                                                            feat_idx,
+                                                            self.abs_corr_mat_)
+            X, estimator = self._impute_one_feature(
+                X, self.mask_missing_values_, feat_idx, neighbor_feat_idx,
+                estimator=estimator, fit_mode=True)
+            estimator_triplet = _ImputerTriplet(feat_idx,
+                                                neighbor_feat_idx,
+                                                estimator)
+            self.imputation_sequence_.append(estimator_triplet)
+        
+        return X
+    
+
     def fit_transform(self, X, y=None):
         """Fits the imputer on X and return the transformed X.
 
@@ -563,36 +662,9 @@ class IterativeImputer(_BaseImputer):
         Xt : array-like, shape (n_samples, n_features)
             The imputed input data.
         """
-        self.random_state_ = getattr(self, "random_state_",
-                                     check_random_state(self.random_state))
+        X, Xt, X_indicator = self.initialize_iteration(X)
 
-        if self.max_iter < 0:
-            raise ValueError(
-                "'max_iter' should be a positive integer. Got {} instead."
-                .format(self.max_iter))
-
-        if self.tol < 0:
-            raise ValueError(
-                "'tol' should be a non-negative float. Got {} instead."
-                .format(self.tol)
-            )
-
-        if self.estimator is None:
-            from ..linear_model import BayesianRidge
-            self._estimator = BayesianRidge()
-        else:
-            self._estimator = clone(self.estimator)
-
-        if hasattr(self._estimator, 'random_state'):
-            self._estimator.random_state = self.random_state_
-
-        self.imputation_sequence_ = []
-
-        self.initial_imputer_ = None
-        super()._fit_indicator(X)
-        X_indicator = super()._transform_indicator(X)
-        X, Xt, mask_missing_values = self._initial_imputation(X)
-        if self.max_iter == 0 or np.all(mask_missing_values):
+        if self.max_iter == 0 or np.all(self.mask_missing_values_):
             self.n_iter_ = 0
             return super()._concatenate_indicator(Xt, X_indicator)
 
@@ -600,74 +672,44 @@ class IterativeImputer(_BaseImputer):
         if Xt.shape[1] == 1:
             self.n_iter_ = 0
             return super()._concatenate_indicator(Xt, X_indicator)
-
-        self._min_value = IterativeImputer._validate_limit(
-            self.min_value, "min", X.shape[1])
-        self._max_value = IterativeImputer._validate_limit(
-            self.max_value, "max", X.shape[1])
-
-        if not np.all(np.greater(self._max_value, self._min_value)):
-            raise ValueError(
-                "One (or more) features have min_value >= max_value.")
-
-        # order in which to impute
-        # note this is probably too slow for large feature data (d > 100000)
-        # and a better way would be good.
-        # see: https://goo.gl/KyCNwj and subsequent comments
-        ordered_idx = self._get_ordered_idx(mask_missing_values)
-        self.n_features_with_missing_ = len(ordered_idx)
-
-        abs_corr_mat = self._get_abs_corr_mat(Xt)
-
-        n_samples, n_features = Xt.shape
+        
         if self.verbose > 0:
             print("[IterativeImputer] Completing matrix with shape %s"
                   % (X.shape,))
         start_t = time()
         if not self.sample_posterior:
             Xt_previous = Xt.copy()
-            normalized_tol = self.tol * np.max(
-                np.abs(X[~mask_missing_values])
-            )
+            if self.tol is not None:
+                normalized_tol = self.tol * np.max(
+                    np.abs(X[~self.mask_missing_values_])
+                )
         for self.n_iter_ in range(1, self.max_iter + 1):
-            if self.imputation_order == 'random':
-                ordered_idx = self._get_ordered_idx(mask_missing_values)
-
-            for feat_idx in ordered_idx:
-                neighbor_feat_idx = self._get_neighbor_feat_idx(n_features,
-                                                                feat_idx,
-                                                                abs_corr_mat)
-                Xt, estimator = self._impute_one_feature(
-                    Xt, mask_missing_values, feat_idx, neighbor_feat_idx,
-                    estimator=None, fit_mode=True)
-                estimator_triplet = _ImputerTriplet(feat_idx,
-                                                    neighbor_feat_idx,
-                                                    estimator)
-                self.imputation_sequence_.append(estimator_triplet)
+            
+            Xt = self.next_iteration(Xt)
 
             if self.verbose > 1:
                 print('[IterativeImputer] Ending imputation round '
                       '%d/%d, elapsed time %0.2f'
                       % (self.n_iter_, self.max_iter, time() - start_t))
 
-            if not self.sample_posterior:
+            if not self.sample_posterior and self.tol is not None:
                 inf_norm = np.linalg.norm(Xt - Xt_previous, ord=np.inf,
-                                          axis=None)
+                                        axis=None)
                 if self.verbose > 0:
                     print('[IterativeImputer] '
-                          'Change: {}, scaled tolerance: {} '.format(
-                              inf_norm, normalized_tol))
+                        'Change: {}, scaled tolerance: {} '.format(
+                            inf_norm, normalized_tol))
                 if inf_norm < normalized_tol:
                     if self.verbose > 0:
                         print('[IterativeImputer] Early stopping criterion '
-                              'reached.')
+                            'reached.')
                     break
                 Xt_previous = Xt.copy()
         else:
-            if not self.sample_posterior:
+            if not self.sample_posterior and self.tol is not None:
                 warnings.warn("[IterativeImputer] Early stopping criterion not"
                               " reached.", ConvergenceWarning)
-        Xt[~mask_missing_values] = X[~mask_missing_values]
+        Xt[~self.mask_missing_values_] = X[~self.mask_missing_values_]
         return super()._concatenate_indicator(Xt, X_indicator)
 
     def transform(self, X):
